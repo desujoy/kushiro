9af580eae751e41b0596cb49190c43a4dc452c73
Deep Sharma <deepshar17@gmail.com>
2025-01-08 00:50:06 +0530

    Include the name Kushiro at the center of the navigation bar. And social media icon shifted to the right

--- public/js/main.js
+++ public/js/main.js
@@ -6,3 +6,11 @@ $(function () {
   });
 
 });
+document.addEventListener('DOMContentLoaded', function () {
+  const hamburger = document.getElementById('hamburger');
+  const navLinks = document.getElementById('nav-links');
+
+  hamburger.addEventListener('click', function () {
+    navLinks.classList.toggle('show');
+  });
+});
